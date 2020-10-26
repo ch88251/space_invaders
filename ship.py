d8918541543499f3d8b3baf8a30a5dd5ea8ace3f
chayes <cfhayes16@gmail.com>
2020-10-25 23:02:18 -0600

    Ship now moves in both directions

--- ship.py
+++ ship.py
@@ -7,6 +7,15 @@ class Ship:
         self.image = pygame.image.load('images/ship.png')
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
+        self.moving_left = False
+        self.moving_right = False
+
+    def update(self):
+        """Update the ship's position based on the movement flag."""
+        if self.moving_left:
+            self.rect.x -= 1
+        if self.moving_right:
+            self.rect.x += 1
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
